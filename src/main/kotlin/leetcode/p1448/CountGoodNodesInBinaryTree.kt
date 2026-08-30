8babd415b28c1c08ca0dfad63415f3f55d82adb5
Thomas Tickle <ttickle@gmail.com>
2026-08-30 13:00:58 -0400

    LeetCode 1448: Count Good Nodes In Binary Tree.

---
+++ src/main/kotlin/leetcode/p1448/CountGoodNodesInBinaryTree.kt
@@ -0,0 +1,55 @@
+package leetcode.p1448
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
+
+/**
+ * # 1448. Count Good Nodes in Binary Tree
+ *
+ * Given a binary tree `root`, a node *X* in the tree is named **good** if in the path from the
+ * root to *X* there are no nodes with a value greater than *X*.
+ *
+ * Return the number of **good** nodes in the binary tree.
+ *
+ * Examples:
+ * - Input: `root = [3,1,4,3,null,1,5]`. Output: `4`.
+ *   Explanation:
+ *   Root Node (3) is always a good node.
+ *   Node 4 -> (3,4) is the maximum value in the path starting from the root.
+ *   Node 5 -> (3,4,5) is the maximum value in the path.
+ *   Node 3 -> (3,1,3) is the maximum value in the path.
+ * - Input: `root = [3,3,null,4,2]`. Output: `3`.
+ *   Explanation:
+ *   Node 2 -> (3, 3, 2) is not good, because '3' is higher than it.
+ * - Input: `root = [1]`. Output: `1`.
+ *   Explanation:
+ *   Root is considered good.
+ *
+ * Constraints:
+ * - The number of nodes in the binary tree is in the range `[1, 10^5]`.
+ * - Each node's value is between `[-10^4, 10^4]`.
+ *
+ * [LeetCode 1448: Count Good Nodes in Binary Tree](https://leetcode.com/problems/count-good-nodes-in-binary-tree/)
+ */
+class Solution {
+    fun goodNodes(root: TreeNode?): Int {
+        fun countGoodNodes(node: TreeNode?, maxSoFar: Int): Int {
+            if (node == null) {
+                return 0
+            }
+
+            val isGood = if (node.`val` >= maxSoFar) 1 else 0
+            val newMax = maxOf(node.`val`, maxSoFar)
+
+            return isGood + countGoodNodes(node.left, newMax) + countGoodNodes(node.right, newMax)
+        }
+
+        return if (root == null) {
+            0
+        } else {
+            countGoodNodes(root, root.`val`)
+        }
+    }
+}
