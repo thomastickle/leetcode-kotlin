790cb6529c7ec5b0ff1943df6ec196d1302066e5
Thomas Tickle <ttickle@gmail.com>
2026-09-20 21:14:49 -0400

    Solution for LeetCode 238: Product of Array Except Self.

--- src/main/kotlin/leetcode/p0238/ProductOfArrayExceptSelf.kt
+++ src/main/kotlin/leetcode/p0238/ProductOfArrayExceptSelf.kt
@@ -26,6 +26,19 @@ package leetcode.p0238
  */
 class Solution {
     fun productExceptSelf(nums: IntArray): IntArray {
-        TODO("Implement solution")
+        val output = IntArray(nums.size)
+
+        var product = 1
+        for (i in nums.indices) {
+            output[i] = product
+            product *= nums[i]
+        }
+
+        product = 1
+        for (i in nums.lastIndex downTo 0) {
+            output[i] *= product
+            product *= nums[i]
+        }
+        return  output
     }
 }
