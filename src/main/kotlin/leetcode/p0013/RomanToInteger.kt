68ac45c2c4272b45a47ffe7c12fcb4b647a85bb8
Thomas Tickle <ttickle@gmail.com>
2026-09-22 22:31:18 -0400

    Solution to LeetCode 13: Roman To Integer

--- src/main/kotlin/leetcode/p0013/RomanToInteger.kt
+++ src/main/kotlin/leetcode/p0013/RomanToInteger.kt
@@ -45,6 +45,27 @@ package leetcode.p0013
  */
 class Solution {
     fun romanToInt(s: String): Int {
-        TODO("Implement solution")
+        fun value(c: Char) = when (c) {
+            'I' -> 1
+            'V' -> 5
+            'X' -> 10
+            'L' -> 50
+            'C' -> 100
+            'D' -> 500
+            'M' -> 1000
+            else -> error("Invalid Roman numeral: $c")
+        }
+
+        var result = 0
+        var previous = 0
+
+        for (i in s.length - 1 downTo 0) {
+            val current = value(s[i])
+
+            result += if (current < previous) -current else current
+            previous = current
+        }
+
+        return result
     }
 }
